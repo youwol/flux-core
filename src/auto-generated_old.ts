45c2e2ba633c2bc488ce98c2a1c57af514202201
greinisch <greinisch@youwol.com>
2021-04-01 07:06:15 +0200

    :art: add auto_generated.ts in .gitignore

--- src/auto-generated_old.ts
+++
@@ -1,7 +0,0 @@
-// This file is auto-generated: do not edit 
-export const ASSET_ID = 'QHlvdXdvbC9mbHV4LWNvcmU=' 
-export const NAMESPACE = 'youwol' 
-export const NAME = '@youwol/flux-core' 
-export const VERSION = '0.0.0-next' 
-export const DESCRIPTION = 'Core library to create flux applications' 
-export const URL_CDN = '/api/assets-gateway/raw/package/QHlvdXdvbC9mbHV4LWNvcmU=/libraries/youwol/flux-core/0.0.0-next' 
--- src/auto_generated.ts
+++
@@ -1,6 +0,0 @@
-export let AUTO_GENERATED = {
-    name: "@youwol/flux-core",
-    namespace: "youwol",
-    version: "0.0.0-next",
-    description: "undefined"
-}
