40af3945fdedac915e14ace651d3dff0e44ff3c8
greinisch <greinisch@youwol.com>
2021-06-03 10:38:11 +0200

    :zap: render-html => provide options : {applyWrapperAttributes}

--- src/lib/models/render-html.ts
+++ src/lib/models/render-html.ts
@@ -8,15 +8,19 @@ import { Component } from '../modules/component.module';
  * @param div the wrapper (parent) div of the target flux module  
  * @param mdle the target module
  */
-function applyWrapperDivAttributes(div: HTMLDivElement, mdle:ModuleFlux){
+function applyWrapperDivAttributes(
+    div: HTMLDivElement, 
+    mdle:ModuleFlux,
+    {applyWrapperAttributes} : {applyWrapperAttributes: boolean} = {applyWrapperAttributes: true}){
 
     let attr =  mdle.Factory.RenderView.wrapperDivAttributes || (() => {})
   
     div.classList.add(`flux-element`)
     mdle instanceof Component.Module && div.classList.add(`flux-component`)
-    attr(mdle) && attr(mdle).class && div.classList.add(attr(mdle).class)
+
+    applyWrapperAttributes && attr(mdle) && attr(mdle).class && div.classList.add(attr(mdle).class)
     
-    let styles = attr(mdle) && attr(mdle).style ?  attr(mdle).style: {}
+    let styles = applyWrapperAttributes && attr(mdle) && attr(mdle).style ?  attr(mdle).style: {}
     Object.entries(styles).forEach( ([k,v]:[string,string])=> div.style.setProperty(k,v))
 }
 
@@ -31,7 +35,10 @@ function applyWrapperDivAttributes(div: HTMLDivElement, mdle:ModuleFlux){
  * view will be considered
  * @returns the input div *templateLayout* with wrapper module's div containing the actual modules' view
  */
-export function renderTemplate( templateLayout: HTMLDivElement, modules: Array<ModuleFlux> ){
+export function renderTemplate( 
+    templateLayout: HTMLDivElement, 
+    modules: Array<ModuleFlux>,
+    options : {applyWrapperAttributes: boolean} = {applyWrapperAttributes: true}){
 
     let modulesToRender =  modules
     .filter( m=> m.Factory.RenderView !== undefined )
@@ -42,7 +49,7 @@ export function renderTemplate( templateLayout: HTMLDivElement, modules: Array<M
     .forEach( ([mdle,renderer]:[ModuleFlux,any]) => { 
         let wrapperDiv = templateLayout.querySelector("#"+mdle.moduleId) as HTMLDivElement
         if(wrapperDiv){
-            applyWrapperDivAttributes(wrapperDiv, mdle)
+            applyWrapperDivAttributes(wrapperDiv, mdle, options)
             let divChild = renderer.render()
             wrapperDiv.appendChild(divChild) 
             if(mdle["renderedElementDisplayed$"])
