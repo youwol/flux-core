1dc13ef17792ac398922332dfedec5227d2b00f6
greinisch <greinisch@youwol.com>
2021-10-04 19:11:47 +0200

    :art: render-html & apply wrapper attributes

--- src/lib/models/render-html.ts
+++ src/lib/models/render-html.ts
@@ -10,22 +10,24 @@ import { Component } from '../modules/component.module';
  */
 function applyWrapperDivAttributes(
     div: HTMLDivElement, 
-    mdle:ModuleFlux,
-    {applyWrapperAttributes} : {applyWrapperAttributes: boolean} = {applyWrapperAttributes: true}){
+    mdle:ModuleFlux
+    ){
+    let attributesGetter =  mdle.Factory.RenderView.wrapperDivAttributes || (() => {})
+    let attributes = attributesGetter(mdle)
 
-    let attr =  mdle.Factory.RenderView.wrapperDivAttributes || (() => {})
-  
     div.classList.add(`flux-element`)
     mdle instanceof Component.Module && div.classList.add(`flux-component`)
-
-    applyWrapperAttributes && attr(mdle) && attr(mdle).class && div.classList.add(attr(mdle).class)
+    let classes = attributes.class
+    if( classes ){
+        div.classList.add(...Array.isArray(classes) ? classes : classes.split(" "))
+    }
     
-    let styles = applyWrapperAttributes && attr(mdle) && attr(mdle).style ?  attr(mdle).style: {}
+    let styles = attributes.style || {}
     Object.entries(styles).forEach( ([k,v]:[string,string])=> div.style.setProperty(k,v))
 }
 
 /**
- * Render a templated layout containing reference to modules' views.
+ * Render a templated layout containing reference to modules' views, substitution is done in-place.
  * 
  * The templated layout, in addition to any regular HTML Element, can contains reference wrapper elements to some modules' view.
  * A reference element is a HTMLDivElement with **id** equal to associated module's id
@@ -47,10 +49,11 @@ export function renderTemplate(
     modulesToRender
     .forEach( ([mdle,renderer]:[ModuleFlux,any]) => { 
         let wrapperDiv = templateLayout.querySelector("#"+mdle.moduleId) as HTMLDivElement
+        
         if(wrapperDiv){
             wrapperDiv.innerHTML = ""
             let divChildren = [renderer.render()].flat()
-            options.applyWrapperAttributes && applyWrapperDivAttributes(wrapperDiv, mdle, options)
+            options.applyWrapperAttributes && applyWrapperDivAttributes(wrapperDiv, mdle)
             divChildren.forEach( (child: HTMLElement)=> wrapperDiv.appendChild(child))
             
             if(mdle["renderedElementDisplayed$"])
