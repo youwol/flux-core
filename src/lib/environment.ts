a2a8277d1ed43c85e119ad816cfa8a5a6f6df6ea
greinisch <greinisch@youwol.com>
2022-01-26 07:22:42 +0100

    :bug: Environment.postProject => use assets-gtw + add cont-type header

--- src/lib/environment.ts
+++ src/lib/environment.ts
@@ -8,276 +8,281 @@ import { FluxPack, HostCommandRequest } from "./models/models-base";
 import { WorkerPool } from "./worker-pool";
 
 
-export function createObservableFromFetch( request, extractFct = (d) =>d ){
+export function createObservableFromFetch(request, extractFct = (d) => d) {
 
     return Observable.create(observer => {
         fetch(request)
-          .then(response => response.json()) // or text() or blob() etc.
-          .then(data => {
-            observer.next( extractFct(data));
-            observer.complete();
-          })
-          .catch(err => observer.error(err));
+            .then(response => response.json()) // or text() or blob() etc.
+            .then(data => {
+                observer.next(extractFct(data));
+                observer.complete();
+            })
+            .catch(err => observer.error(err));
     });
 }
 
 type IConsole = {
-    log:(message?: any, ...optionalParams: any[])=> void,
-    warn:(message?: any, ...optionalParams: any[])=> void,
-    error:(message?: any, ...optionalParams: any[])=> void,
+    log: (message?: any, ...optionalParams: any[]) => void,
+    warn: (message?: any, ...optionalParams: any[]) => void,
+    error: (message?: any, ...optionalParams: any[]) => void,
 }
 
-export enum ProcessMessageKind{
+export enum ProcessMessageKind {
     Scheduled = "Scheduled",
     Started = "Started",
     Canceled = "Canceled",
     Failed = "Failed",
-    Succeeded= "Succeeded",
-    Log= "Log"
+    Succeeded = "Succeeded",
+    Log = "Log"
 
 }
-export interface ProcessMessage{
+export interface ProcessMessage {
 
     kind: ProcessMessageKind
     text: string
 }
 
-export class Process{
+export class Process {
 
     public readonly messages$ = new Subject<ProcessMessage>()
 
     constructor(
         public readonly title: string,
         public readonly context?: Context
-        ){
-        }
-    
-    schedule(text: string = "scheduled"){
-        this.messages$.next({kind:ProcessMessageKind.Scheduled, text: text})
+    ) {
     }
-    start(text: string = "Started"){
-        this.messages$.next({kind:ProcessMessageKind.Started, text: text})
+
+    schedule(text: string = "scheduled") {
+        this.messages$.next({ kind: ProcessMessageKind.Scheduled, text: text })
     }
-    cancel(text: string = "Canceled"){
-        this.messages$.next({kind:ProcessMessageKind.Canceled, text: text})
+    start(text: string = "Started") {
+        this.messages$.next({ kind: ProcessMessageKind.Started, text: text })
+    }
+    cancel(text: string = "Canceled") {
+        this.messages$.next({ kind: ProcessMessageKind.Canceled, text: text })
         this.messages$.complete()
     }
-    fail(text: string = "Failed"){
-        this.messages$.next({kind:ProcessMessageKind.Failed, text: text})
+    fail(text: string = "Failed") {
+        this.messages$.next({ kind: ProcessMessageKind.Failed, text: text })
         this.messages$.complete()
     }
-    succeed(text: string = "Succeeded"){
-        this.messages$.next({kind:ProcessMessageKind.Succeeded, text: text})
+    succeed(text: string = "Succeeded") {
+        this.messages$.next({ kind: ProcessMessageKind.Succeeded, text: text })
         this.messages$.complete()
     }
-    log(text: string){
-        this.messages$.next({kind:ProcessMessageKind.Log, text: text})
+    log(text: string) {
+        this.messages$.next({ kind: ProcessMessageKind.Log, text: text })
     }
 }
 
-export interface IEnvironment{
+export interface IEnvironment {
 
     console: IConsole
-    
-    errors$ : Subject<ErrorLog>
-    
-    hostCommandRequest$ : Subject<HostCommandRequest>
 
-    processes$ : ReplaySubject<Process>
+    errors$: Subject<ErrorLog>
 
-    workerPool : WorkerPool
+    hostCommandRequest$: Subject<HostCommandRequest>
 
-    fetchStyleSheets( resources: string | Array<string>) : Observable<Array<HTMLLinkElement>>
+    processes$: ReplaySubject<Process>
 
-    fetchJavascriptAddOn( resources: string | Array<string> , onEvent?: (CdnEvent) => void): Observable<{assetName, assetId, url, src}[]>
+    workerPool: WorkerPool
 
-    fetchLoadingGraph(loadingGraph: LoadingGraphSchema, onEvent?: (CdnEvent) => void) : Observable<LoadingGraphSchema>
-    
-    fetchSources(sources: {name: string, assetId: string, url: string}[], onEvent?:  ( event: CdnEvent) => void ) : Observable<{name, assetId, url, content}[]> 
+    fetchStyleSheets(resources: string | Array<string>): Observable<Array<HTMLLinkElement>>
 
-    getProject(projectId) : Observable<ProjectSchema>
+    fetchJavascriptAddOn(resources: string | Array<string>, onEvent?: (CdnEvent) => void): Observable<{ assetName, assetId, url, src }[]>
 
-    postProject(projectId:string, project:Object ) : Observable<void> 
+    fetchLoadingGraph(loadingGraph: LoadingGraphSchema, onEvent?: (CdnEvent) => void): Observable<LoadingGraphSchema>
 
-    getLoadingGraph({libraries}:{libraries:{[key:string]: string}}) : Observable<LoadingGraphSchema>
+    fetchSources(sources: { name: string, assetId: string, url: string }[], onEvent?: (event: CdnEvent) => void): Observable<{ name, assetId, url, content }[]>
 
-    exposeProcess({title, context}:{
-        title:string, 
+    getProject(projectId): Observable<ProjectSchema>
+
+    postProject(projectId: string, project: Object): Observable<void>
+
+    getLoadingGraph({ libraries }: { libraries: { [key: string]: string } }): Observable<LoadingGraphSchema>
+
+    exposeProcess({ title, context }: {
+        title: string,
         context?: Context
-        }): Process
+    }): Process
 
 }
 
-export class Environment implements IEnvironment{
+export class Environment implements IEnvironment {
 
     public readonly executingWindow: Window
     public readonly renderingWindow: Window
 
     public readonly errors$ = new ReplaySubject<ErrorLog>()
-    public readonly hostCommandRequest$ = new  Subject<HostCommandRequest>()
+    public readonly hostCommandRequest$ = new Subject<HostCommandRequest>()
     public readonly processes$ = new ReplaySubject<Process>()
 
     public readonly console: IConsole
 
-    public readonly workerPool : WorkerPool
-    
-    constructor( data:
-                {executingWindow: Window, renderingWindow: Window , console?: Console }){
+    public readonly workerPool: WorkerPool
+
+    constructor(data:
+        { executingWindow: Window, renderingWindow: Window, console?: Console }) {
 
         this.renderingWindow = data.renderingWindow
         this.executingWindow = data.executingWindow
-        this.workerPool = new WorkerPool({environment:this})
+        this.workerPool = new WorkerPool({ environment: this })
         this.console = data.console || console
     }
 
-    fetchStyleSheets( resources: string | Array<string>) : Observable<Array<HTMLLinkElement>>{
+    fetchStyleSheets(resources: string | Array<string>): Observable<Array<HTMLLinkElement>> {
 
         return from(fetchStyleSheets(resources, this.renderingWindow))
     }
 
-    fetchJavascriptAddOn( resources: string | Array<string> , onEvent?: (CdnEvent) => void): Observable<{assetName, assetId, url, src}[]>{
+    fetchJavascriptAddOn(resources: string | Array<string>, onEvent?: (CdnEvent) => void): Observable<{ assetName, assetId, url, src }[]> {
 
-        return from(fetchJavascriptAddOn( resources, this.executingWindow, onEvent))
+        return from(fetchJavascriptAddOn(resources, this.executingWindow, onEvent))
     }
 
-    fetchLoadingGraph(loadingGraph: LoadingGraph , onEvent?: (CdnEvent) => void) : Observable<LoadingGraphSchema> {
+    fetchLoadingGraph(loadingGraph: LoadingGraph, onEvent?: (CdnEvent) => void): Observable<LoadingGraphSchema> {
 
         return from(fetchLoadingGraph(loadingGraph, this.executingWindow, undefined, onEvent)).pipe(
-            map( () => loadingGraph)
+            map(() => loadingGraph)
         )
     }
 
-    fetchSources(sources: {name: string, assetId: string, url: string}[], onEvent?:  ( event: CdnEvent) => void ) : Observable<{name, assetId, url, content}[]> {
+    fetchSources(sources: { name: string, assetId: string, url: string }[], onEvent?: (event: CdnEvent) => void): Observable<{ name, assetId, url, content }[]> {
 
-        let promises = sources.map( ({name, assetId, url}) =>  fetchSource( name, assetId, url, onEvent))
+        let promises = sources.map(({ name, assetId, url }) => fetchSource(name, assetId, url, onEvent))
         return forkJoin(promises)
     }
 
-    getProject(projectId) : Observable<ProjectSchema> {
+    getProject(projectId): Observable<ProjectSchema> {
 
-        let url =  `/api/assets-gateway/raw/flux-project/${projectId}`
-        var request = new Request(url, {  method: 'GET', headers: {}, });
-        return createObservableFromFetch(request)
-    }
-    
-    postProject(projectId:string, project:Object ) : Observable<void> {
-
-        let url = `/api/flux-backend/projects/${projectId}`
-        var request = new Request(url, {  method: 'POST', headers: {}, body : JSON.stringify(project)});
-        
+        let url = `/api/assets-gateway/raw/flux-project/${projectId}`
+        var request = new Request(url, { method: 'GET', headers: {}, });
         return createObservableFromFetch(request)
     }
 
-    getLoadingGraph(body:{libraries:{[key:string]: string}}) : Observable<LoadingGraphSchema> {
-    
+    postProject(projectId: string, project: Object): Observable<void> {
+
+        let url = `/api/assets-gateway/raw/flux-project/${projectId}`
+        var request = new Request(url, {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(project)
+        }
+        );
+
+        return createObservableFromFetch(request)
+    }
+
+    getLoadingGraph(body: { libraries: { [key: string]: string } }): Observable<LoadingGraphSchema> {
+
         return from(getLoadingGraph(body))
     }
 
-    exposeProcess({title, context}:{
-        title:string, 
+    exposeProcess({ title, context }: {
+        title: string,
         context?: Context
-        }): Process {
-            let p = new Process(
-                title,
-                context
-                )
-            this.processes$.next(p)
-            return p 
-        }
+    }): Process {
+        let p = new Process(
+            title,
+            context
+        )
+        this.processes$.next(p)
+        return p
+    }
 }
 
-export type KeyLoadingGraphStore = {libraries:{[key:string]: string}, using:{[key:string]: string}}
+export type KeyLoadingGraphStore = { libraries: { [key: string]: string }, using: { [key: string]: string } }
 
-export class MockEnvironment implements IEnvironment{
+export class MockEnvironment implements IEnvironment {
 
     css = new Array<string>()
     jsAddons = new Array<string>()
-    savedProjects : {[key:string]: ProjectSchema} = {}
+    savedProjects: { [key: string]: ProjectSchema } = {}
 
-    public readonly projectsDB: {[key:string]: ProjectSchema}
-    public readonly fluxPacksDB: {[key:string]: FluxPack}
-    public readonly loadingGraphResponses: {[key:string]: LoadingGraphSchema}
+    public readonly projectsDB: { [key: string]: ProjectSchema }
+    public readonly fluxPacksDB: { [key: string]: FluxPack }
+    public readonly loadingGraphResponses: { [key: string]: LoadingGraphSchema }
     public readonly console: IConsole
 
     public readonly errors$ = new ReplaySubject<ErrorLog>()
-    public readonly hostCommandRequest$ = new  Subject<HostCommandRequest>()
+    public readonly hostCommandRequest$ = new Subject<HostCommandRequest>()
     public readonly processes$ = new ReplaySubject<Process>()
-    
+
     public readonly workerPool = undefined
 
     constructor(
         data: {
-            projectsDB?: {[key:string]: ProjectSchema},
+            projectsDB?: { [key: string]: ProjectSchema },
             fluxPacks?: Array<FluxPack>,
             console?: IConsole,
             loadingGraphResponses?: Array<[KeyLoadingGraphStore, LoadingGraphSchema]>
-        } = {}){
+        } = {}) {
         this.projectsDB = data.projectsDB || {}
-        this.fluxPacksDB = data.fluxPacks 
-            ? data.fluxPacks.reduce((acc,e) => ({...acc, ...{[e.name]:e}}), {}) 
+        this.fluxPacksDB = data.fluxPacks
+            ? data.fluxPacks.reduce((acc, e) => ({ ...acc, ...{ [e.name]: e } }), {})
             : {}
-        this.loadingGraphResponses = data.loadingGraphResponses 
-            ? data.loadingGraphResponses.reduce((acc,[k,v]) => ({...acc, ...{[JSON.stringify(k)]:v}}), {}) 
-            : {} 
+        this.loadingGraphResponses = data.loadingGraphResponses
+            ? data.loadingGraphResponses.reduce((acc, [k, v]) => ({ ...acc, ...{ [JSON.stringify(k)]: v } }), {})
+            : {}
         this.console = data.console || console
     }
 
-    fetchStyleSheets( resources: string | Array<string>) : Observable<Array<HTMLLinkElement>>{
+    fetchStyleSheets(resources: string | Array<string>): Observable<Array<HTMLLinkElement>> {
 
         resources = Array.isArray(resources) ? resources : [resources]
         this.css = [...this.css, ...resources]
         return of(resources as unknown as Array<HTMLLinkElement>)
     }
 
-    fetchJavascriptAddOn( resources: string | Array<string> ): Observable<{assetName, assetId, url, src}[]>{
+    fetchJavascriptAddOn(resources: string | Array<string>): Observable<{ assetName, assetId, url, src }[]> {
 
         resources = Array.isArray(resources) ? resources : [resources]
-        let parsed = resources.map( (r) => parseResourceId(r))
+        let parsed = resources.map((r) => parseResourceId(r))
         this.jsAddons = [...this.jsAddons, ...resources]
-        return of(parsed.map( ({name, url, assetId}) => {
-            return {assetName:name, assetId, url, src:""}
+        return of(parsed.map(({ name, url, assetId }) => {
+            return { assetName: name, assetId, url, src: "" }
         }))
     }
 
-    fetchLoadingGraph(loadingGraph: LoadingGraphSchema) : Observable<LoadingGraphSchema>{
+    fetchLoadingGraph(loadingGraph: LoadingGraphSchema): Observable<LoadingGraphSchema> {
         loadingGraph.lock.forEach(
             (resource) => {
-                window[resource.name] = {pack:this.fluxPacksDB[resource.name]}
+                window[resource.name] = { pack: this.fluxPacksDB[resource.name] }
             }
         )
         return of(loadingGraph)
     }
-    
-    fetchSources(sources: {name: string, assetId: string, url: string}[], onEvent?:  ( event: CdnEvent) => void ) : Observable<{name, assetId, url, content}[]>{
-        throw Error(`fetchSource not implemented in MockEnvironment` )
+
+    fetchSources(sources: { name: string, assetId: string, url: string }[], onEvent?: (event: CdnEvent) => void): Observable<{ name, assetId, url, content }[]> {
+        throw Error(`fetchSource not implemented in MockEnvironment`)
     }
 
-    getProject(projectId: string) : Observable<ProjectSchema>{
+    getProject(projectId: string): Observable<ProjectSchema> {
 
-        if(!this.projectsDB[projectId]){
+        if (!this.projectsDB[projectId]) {
             throw Error(`The project with id ${projectId} can not be found `)
         }
         return of(this.projectsDB[projectId])
     }
 
-    postProject(projectId:string, project: ProjectSchema ) : Observable<void> {
+    postProject(projectId: string, project: ProjectSchema): Observable<void> {
         this.savedProjects[projectId] = project
         return of()
     }
 
-    getLoadingGraph(body: KeyLoadingGraphStore) : Observable<LoadingGraphSchema> {
+    getLoadingGraph(body: KeyLoadingGraphStore): Observable<LoadingGraphSchema> {
 
         let key = JSON.stringify(body)
-        if(!this.loadingGraphResponses[key])
-            throw Error(`MockEnvironment.getLoadingGraph: loading graph not found for key: ${key}` )
+        if (!this.loadingGraphResponses[key])
+            throw Error(`MockEnvironment.getLoadingGraph: loading graph not found for key: ${key}`)
 
         return of(this.loadingGraphResponses[key])
     }
 
-    exposeProcess({title, context}:{
-        title:string, 
+    exposeProcess({ title, context }: {
+        title: string,
         context?: Context
-        }): Process {
-            throw Error(`exposeProcess not implemented in MockEnvironment` )
-        }
+    }): Process {
+        throw Error(`exposeProcess not implemented in MockEnvironment`)
+    }
 }
