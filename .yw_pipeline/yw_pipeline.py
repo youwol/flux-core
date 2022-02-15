a68eb69b57a2bc0ab92553be183ef710f9544e33
greinisch <greinisch@youwol.com>
2022-02-15 15:16:24 +0100

    🔨 sync yw_pipeline

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -1,6 +1,7 @@
 from youwol.environment.forward_declaration import YouwolEnvironment
 from youwol.environment.models import IPipelineFactory
-from youwol.pipelines.pipeline_typescript_weback_npm import pipeline
+from youwol.environment.models_project import JsBundle
+from youwol.pipelines.pipeline_typescript_weback_npm import pipeline, PipelineConfig
 from youwol_utils.context import Context
 
 
@@ -9,5 +10,8 @@ class PipelineFactory(IPipelineFactory):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    async def get(self, env: YouwolEnvironment, ctx: Context):
-        return pipeline()
+    async def get(self, env: YouwolEnvironment, context: Context):
+        config = PipelineConfig(
+            target=JsBundle()
+        )
+        return await pipeline(config, context)
